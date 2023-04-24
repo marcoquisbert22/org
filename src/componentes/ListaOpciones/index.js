72846bb77470a34d136da4b2a56f2ba7cda7d9bc
Marco Antonio <marcoquisbert22@gmail.com>
2023-04-23 21:25:39 -0400

    nuevos Cambios

---
+++ src/componentes/ListaOpciones/index.js
@@ -0,0 +1,26 @@
+import "./ListaOpciones.css"
+const ListaOpciones = (props) => {
+
+        // // Metodo map > arreglo.map (()=>{
+
+
+        // })
+        
+        
+
+
+    const manejarCambio = (e) =>{
+        console.log("cambio",e)
+        props.actualizarEquipo (e.target.value)
+    }
+
+    return <div className="lista-opciones">
+        <label>Equipos</label>
+        <select value = {props.valor} onChange={manejarCambio}>
+            <option value ="" disabled defaultValue ="" hidden>Seleccionar equipo</option>
+            {props.equipos.map ((equipos,index)=> <option key={index} value = {equipos} > {equipos} </option>)}
+        </select>
+</div>
+}
+
+export default ListaOpciones
---
+++ src/componentes/header/Header.js
@@ -0,0 +1,10 @@
+import "./Header.css"
+
+function Header(){
+    return <header className="header">
+        <img src='/img/Header@2x.png' alt='Org'/>
+        </header> 
+}
+        
+
+export default Header
